5f301fb041c20753a226f1f5cf347979060dd117
Nick Cox <nick@nickcox.me>
2021-11-29 22:15:31 -0800

    Convert HTML building to lodash.template from raw js

--- app/frontend/packs/mainjs.js
+++ app/frontend/packs/mainjs.js
@@ -1,13 +1,15 @@
+import template from 'lodash.template';
+
 const products = async function() { 
   const id = document.getElementById('product-display').getAttribute('data-id');
   const req = await fetch(`/products/${id}.json`);
-  const productsJson = await req.json(); 
+  const productJson = await req.json(); 
+
+  const compiledTemplate = template('<h1>foo <%= title %></h1>');
  
-  console.log(productsJson);
-  const body = document.getElementsByTagName('body')[0];
-  const productName = productsJson.title;
-  const headline = document.createElement('h1');
-  headline.innerText = productName;
-  body.appendChild(headline);
+  const container = document.createElement('div');
+  container.innerHTML = compiledTemplate(productJson);
+
+  document.getElementsByTagName('body')[0].appendChild(container);
 }();
 
